ec992607fb6f1e964b65c3f6f3be5a23f9fcf7fd
Rodrigo Lopes <rdg-lopes@uol.com.br>
2022-05-28 16:39:27 -0300

    Inclusão do load dos destination na ActivityMain

--- app/src/main/java/com/mapstogo/pucprmaps/MainActivity.java
+++ app/src/main/java/com/mapstogo/pucprmaps/MainActivity.java
@@ -3,12 +3,27 @@ package com.mapstogo.pucprmaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<DestinationModelView> listDestinationLoaded = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadDestinations();
     }
+
+    private void loadDestinations() {
+        List<DestinationModelView> listLoaded = new DestinationModelViewMemoryFactory().getAllDestinations();
+        this.listDestinationLoaded.addAll(listLoaded);
+    }
+
 }
