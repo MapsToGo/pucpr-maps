6311623eaac8d91fd1bb87dd2ffdcc95d2c78efe
Rodrigo Lopes <rdg-lopes@uol.com.br>
2022-05-28 18:37:58 -0300

    Impl do filtro dos destinations na pesquisa

--- app/src/main/java/com/mapstogo/pucprmaps/MainActivity.java
+++ app/src/main/java/com/mapstogo/pucprmaps/MainActivity.java
@@ -13,12 +13,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<DestinationModelView> listDestinationLoaded = new ArrayList<>();
+    private List<DestinationModelView> listDestinationsFound = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadDestinations();
+        configEditText();
     }
 
     private void loadDestinations() {
@@ -26,4 +28,31 @@ public class MainActivity extends AppCompatActivity {
         this.listDestinationLoaded.addAll(listLoaded);
     }
 
+    private void configEditText() {
+        EditText editTextSearch = findViewById(R.id.editTextTextSearch);
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                filterListDestinationFound(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    private void filterListDestinationFound(CharSequence charSequence) {
+        this.listDestinationsFound.clear();
+        String strSearch = charSequence.toString().toLowerCase();
+        for(DestinationModelView dest : this.listDestinationLoaded){
+            if(dest.getName().toLowerCase().contains(strSearch)){
+                this.listDestinationsFound.add(dest);
+            }
+        }
+    }
 }
